e2f6464fc4f64bc6b8926a7e2664d6b7488f3436
dvargas42 <dvargas.eng@gmail.com>
2024-03-30 16:02:07 -0300

    feat: :tada: initial commit

---
+++ src/br/com/alura/gerenciador/acao/AlteraEmpresa.java
@@ -0,0 +1,41 @@
+package br.com.alura.gerenciador.acao;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
+
+public class AlteraEmpresa implements Acao {
+	
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String paramId = request.getParameter("id");
+		String nomeEmpresa = request.getParameter("nome");
+		String paramDataAbertura =  request.getParameter("data");
+		
+		System.out.println(paramId);
+		Integer id = Integer.parseInt(paramId);
+		
+		Date dataAbertura = null;
+
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			dataAbertura = sdf.parse(paramDataAbertura);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+		
+		Banco banco = new Banco();
+		Empresa empresa = banco.getEmpresaPeloId(id);
+		empresa.setNome(nomeEmpresa);
+		empresa.setDataAbertura(dataAbertura);
+		
+		return "redirect:entrada?acao=ListaEmpresas";
+	}
+}
