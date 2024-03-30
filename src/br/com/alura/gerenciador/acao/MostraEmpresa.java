fa00e02153f164187c49c53e6ef60300c54ca888
dvargas42 <dvargas.eng@gmail.com>
2024-03-30 16:02:18 -0300

    feat: :tada: initial commit

---
+++ src/br/com/alura/gerenciador/acao/MostraEmpresa.java
@@ -0,0 +1,25 @@
+package br.com.alura.gerenciador.acao;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
+
+public class MostraEmpresa implements Acao {
+	
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String paramId = request.getParameter("id");
+		Integer id = Integer.valueOf(paramId);
+		
+		Banco banco = new Banco();
+		Empresa empresa = banco.getEmpresaPeloId(id);
+		request.setAttribute("empresa", empresa);
+		
+		return "forward:alteraEmpresa.jsp";
+	}
+}
