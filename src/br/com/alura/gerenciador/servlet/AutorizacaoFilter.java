e5e872c9ca98cfb178627864d7aa16f9880ef2f7
dvargas42 <dvargas.eng@gmail.com>
2024-04-01 13:17:43 -0300

    feat: :fire: remove  WebFilter annotations in AutoriazacaoFilter

--- src/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
+++ src/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
@@ -12,8 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-//@WebFilter("/entrada")
 public class AutorizacaoFilter extends HttpFilter implements Filter {
        
 	private static final long serialVersionUID = 1L;
