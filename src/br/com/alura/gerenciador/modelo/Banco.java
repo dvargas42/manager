e2969689f8a6f0595a910671f9466e907ff99e37
dvargas42 <dvargas.eng@gmail.com>
2024-03-31 21:16:51 -0300

    feat: :sparkles: implement usuario logic in Banco class

--- src/br/com/alura/gerenciador/modelo/Banco.java
+++ src/br/com/alura/gerenciador/modelo/Banco.java
@@ -8,6 +8,7 @@ public class Banco {
 	
 	private static Integer chaveSequencial = 1;
 	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
 	
 	static {
 		Empresa empresa = new Empresa();
@@ -20,6 +21,18 @@ public class Banco {
 		
 		lista.add(empresa);
 		lista.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("daniel");
+		u1.setSenha("12345");
+		
+		Usuario u2 = new Usuario();
+		u2.setLogin("lilian");
+		u2.setSenha("12345");
+		
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
+		
 	}
 
 	public void adiciona(Empresa empresa) {
@@ -48,6 +61,15 @@ public class Banco {
 		for (Empresa empresa : lista) {
 			if (id.equals(empresa.getId())) {
 				return empresa;
+			}
+		}
+		return null;
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		for (Usuario usuario : listaUsuarios) {
+			if(usuario.ehIgual(login, senha)) {
+				return usuario;
 			}
 		}
 		return null;
